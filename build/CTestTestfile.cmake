def9923b6ede2344bf57d1ae3c0f12987ada85aa
Kairav Parekh <85573509+kpp16@users.noreply.github.com>
2023-06-01 00:15:47 -0400

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/Projects/K2Assembly/Assembler
-# Build directory: /mnt/d/Projects/K2Assembly/Assembler/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
